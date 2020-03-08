40f985eeb7c4bda86b8cb16af099af0e386f229c
Amjad Abujamous <44087932+glorious73@users.noreply.github.com>
2020-03-08 15:48:47 +0300

    Toggle side nav with JS.

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('nav').style.left = '0px';
+    document.getElementById('openButton').style.display = 'none';
+
+}
+function closeNav() {
+    document.getElementById('nav').style.left = '-200px';
+    document.getElementById('openButton').style.display = 'block';
+}
